a7c4ae8b52c6ea8d15bbd69e8702c482e4d777b7
DaylenWhite <wijesooriyak50516@masseyhigh.school.nz>
2026-06-12 12:22:09 +1200

    Variable Basics

---
+++ P_02_variables.py
@@ -0,0 +1,15 @@
+#Code to learn about variables...
+
+#Vareiables can be a string (note the speech marks)
+greeting = "hello world"
+
+#Variables can be a number
+fav_num = 7
+num_string = "5"
+
+print(fav_num * 2)
+print(num_string * 5)
+
+#Constants are uppercase
+PI = 3.14
+
